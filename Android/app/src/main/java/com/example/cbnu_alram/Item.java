f1a5d6b0c9cd8e4dc0a1f4ce0a0d5cdfef3aef8c
nohgijin <ynda11111@naver.com>
2020-05-30 15:40:53 +0900

    Item class getter setter

---
+++ Android/app/src/main/java/com/example/cbnu_alram/Item.java
@@ -0,0 +1,35 @@
+package com.example.cbnu_alram;
+
+public class Item {
+
+    private String name;
+    private String contents;
+    private String date;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+}
