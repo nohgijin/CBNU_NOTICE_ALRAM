833a31a56deeb18cda1e26f7cdbc40ad05bc2cd8
nohgijin <ynda11111@naver.com>
2020-06-02 19:50:35 +0900

    공지사항 api 및 query

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const {
+  promisePool
+} = require('./db')
 var router = express.Router();
+var { format } = require('mysql2')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/notice', async (req, res) => {
+  // console.log(req.query.site)
+  let q = format(`SELECT * FROM notice_detail_no_contents WHERE site_id in (${req.query.site}) order by date desc`)
+  // console.log(q)
+  let data = await promisePool.query(q)
+  rows = data[0]
+  res.json(rows)
 });
+
 
 module.exports = router;
