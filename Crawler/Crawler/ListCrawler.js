eccafdb49420ace1f916c6182aae5c20159e580d
jaryapp <jaryapp@gmail.com>
2020-06-03 20:48:35 +0900

    ListCrawler Class

---
+++ Crawler/Crawler/ListCrawler.js
@@ -0,0 +1,63 @@
+const puppeteer = require('puppeteer');
+
+
+const {
+    promisePool
+} = require('../db')
+
+
+
+class Crawler {
+    constructor(siteList) {
+        this.siteList = siteList
+        this.init()
+    }
+
+    async init() {
+        // 브라우저 초기화
+        this.browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                `--window-size=${ 1920 },${ 1080 }`,
+            ],
+        });
+
+        // 페이지 탭 개수 결정 사이트를 10개단위로 나눠서 수집
+        var tabCnt = Math.ceil(this.siteList.length / 10)
+
+        for (var t = 0; t < tabCnt; t++) {
+            // 페이지 생성
+            var page = await this.browser.newPage();
+
+            await page.setRequestInterception(true);
+
+            // 페이지 기본설정
+            await page.setViewport({
+                width: 1920,
+                height: 1080,
+                deviceScaleFactor: 1,
+            });
+
+            // 다이어로그 메시지 무시
+            page.on('dialog', async dialog => {
+                await dialog.dismiss();
+            });
+
+            page.on('request', (request) => {
+                if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
+                    request.abort();
+                } else {
+                    request.continue();
+                }
+            });
+
+
+        }
+
+
+    }
+
+
+}
+
+module.exports = Crawler
