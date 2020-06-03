f88a275fe0e54a8be6310f18737747602a0868b0
jaryapp <jaryapp@gmail.com>
2020-06-03 21:42:35 +0900

    크롤링 스크립트 불러오기

--- Crawler/index.js
+++ Crawler/index.js
@@ -1,35 +1,24 @@
-const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('https://software.cbnu.ac.kr/bbs/bbs.php?db=notice&pgID=ID12415888101');
+var fs = require('fs');
 
-    var data = await page.evaluate(() => {
-        getData: function getData() {
-            var list = document.querySelectorAll('#content > table:nth-child(8) > tbody > tr')
-            let i = 3;
-            let data = []
-            while (i < list.length) {
-                let td = list[i].querySelectorAll('td')
-                data.push({
-                    site: "소프트웨어학과",
-                    category: "공지사항",
-                    site_id: 130101,
-                    title: td[2].innerText.trim(),
-                    url: td[2].querySelector('a').href.trim(),
-                    date: td[6].innerText.trim()
-                })
-                i++
-            }
-            return data
-        }
-        return getData()
-    })
+var Crawler = require('./Crawler/ListCrawler')
 
-    console.log(data)
+const HOME = `./script`
 
-    await browser.close();
-})();
+var dirList = fs.readdirSync(HOME)
+var siteList = []
+
+
+for (const dir of dirList) {
+    if (dir == '.git') continue
+    var dirPath = HOME + '/' + dir
+    if (dir.includes('.')) continue
+    var fileList = fs.readdirSync(dirPath)
+    for (const file of fileList) {
+        if (!file.includes('.js')) continue
+        var filePath = dirPath + '/' + file
+        siteList.push(require(filePath))
+    }
+}
+
+new Crawler(siteList)
