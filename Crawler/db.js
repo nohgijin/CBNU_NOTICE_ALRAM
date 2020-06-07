118a304d60bb6d116d22ec53750f40c94bf6199c
jaryapp <jaryapp@gmail.com>
2020-06-08 01:12:18 +0900

    수집한 게시물 데이터베이스에 저장

--- Crawler/db.js
+++ Crawler/db.js
@@ -11,5 +11,10 @@ module.exports = {
         const [rows, fields] = await promisePool.query('SELECT * FROM notice WHERE title = ? and url = ?', [notice.title, notice.url])
         if (rows.length == 0) return false
         else return true
-    }
+    },
+    async insertNotice({site_id,title,url,date}){
+        if(date == "") date=null
+        const [result] = await promisePool.query('INSERT INTO notice SET ?', {site_id,title,url,date})
+        return result;
+    },
 }
