e424ded81d294f30060d45b5adf2d2b6de7e2655
jaryapp <jaryapp@gmail.com>
2020-06-10 14:38:13 +0900

    okhttp 의존성, request 함수 구현

--- Android/app/src/main/java/com/example/cbnu_alram/MainActivity.java
+++ Android/app/src/main/java/com/example/cbnu_alram/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.cbnu_alram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    Call getNoticeList(String url, String json, Callback callback) {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+//                .header("token",fcm_token)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
     }
 }
