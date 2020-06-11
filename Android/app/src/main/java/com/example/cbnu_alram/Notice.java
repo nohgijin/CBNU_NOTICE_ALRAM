0beb3080662b090a7b359387747b26ee730a7a2c
jaryapp <jaryapp@gmail.com>
2020-06-12 03:53:15 +0900

    웹뷰에 컨텐츠 내용 HTML로 띄우기

--- Android/app/src/main/java/com/example/cbnu_alram/Notice.java
+++ Android/app/src/main/java/com/example/cbnu_alram/Notice.java
@@ -37,7 +37,29 @@ public class Notice  extends Activity {
         setContentView(R.layout.notice);
 
         Intent intent = getIntent();
-        
+
+
+        // 웹뷰 시작
+        WebView mWebView = (WebView) findViewById(R.id.notice_contents);
+        WebSettings mWebSettings;
+
+
+        mWebView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        //cookieManager.setAcceptThirdPartyCookies(mWebView, true);
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용
+        mWebSettings.setAppCacheEnabled(true);
 
         String notice_id = intent.getStringExtra("notice_id");
 
