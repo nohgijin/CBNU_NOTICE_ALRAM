608492306aa5389717741b07ef7e5423d6878de0
nohgijin <ynda11111@naver.com>
2020-06-12 17:44:44 +0900

    안전공학과, 토목공학부 크롤링

---
+++ Crawler/script/공과대학/안전공학과.js
@@ -0,0 +1,30 @@
+module.exports = {
+    url: "http://safety.cbnu.ac.kr/index.php?mid=safety_sub04",
+    site_id: 20701,
+    getData: function getData() {
+        var list = document.querySelectorAll('div.bd_lst_wrp > table > tbody > tr')
+        let i = 0;
+        let data = []
+        while (i < list.length) {
+            let td = list[i].querySelectorAll('td')
+            data.push({
+                site: "안전공학과",
+                category: "공지사항",
+                site_id:20701,
+                title: td[1].innerText.trim(),
+                url: td[1].querySelector('a').href.trim(),
+                date: td[3].innerText.trim(),
+            })
+            i++;
+        }
+        return data
+    },
+    getContentsHtml: function getContentsHtml(){
+        let IMG=document.querySelectorAll('img')
+        for(img of IMG){
+            img.src=img.src
+        }
+     
+        return document.querySelector('#content > div.content2 > div.bd.hover_effect > div.rd.rd_nav_style2.clear > div.rd_body.clear').outerHTML
+    },    
+}
