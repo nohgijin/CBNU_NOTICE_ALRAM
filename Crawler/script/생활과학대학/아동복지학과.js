853d8e4f9f56ee47af2052d0edd668ee9cd96abb
jaryapp <jaryapp@gmail.com>
2020-06-14 01:40:42 +0900

    식품영양학과 아동복지학과 크롤링

---
+++ Crawler/script/생활과학대학/아동복지학과.js
@@ -0,0 +1,32 @@
+module.exports = {
+    url: "http://childwelfare.cbnu.ac.kr/main/sub.html?pageCode=26",
+    site_id: 60301,
+    getData: function getData() {
+        var list = document.querySelectorAll('#AB_SKIN > div > div.AB_list > ul > li > table > tbody > tr')
+        let i = 0;
+        let data = []
+        while (i < list.length-1) {
+            let td = list[i].querySelectorAll('td')
+            let urltmp=td[1].querySelector('a').onclick.toString().split("'")[3]
+            let urla=`http://childwelfare.cbnu.ac.kr/main/sub.html?Mode=view&boardID=www26&num=${urltmp}`
+            data.push({
+                site: "아동복지학과",
+                category: "공지사항",
+                site_id:60301,
+                title: td[1].querySelector('a').innerText.trim(),
+                url: urla,
+                date: td[3].querySelector('p').innerText.trim()
+            })
+            i++;
+        }
+        return data
+    },
+   getContentsHtml: function getContentsHtml() {
+
+       let IMG = window.frames[0].document.querySelectorAll('img')
+       for (img of IMG) {
+           img.src = img.src
+       }
+       return window.frames[0].document.querySelector('body').innerHTML
+   },
+}
