c1a02964901cd50c7cf86b359485c0bf330aa6e7
nohgijin <ynda11111@naver.com>
2020-06-13 19:59:06 +0900

    생물교육과, 수학교육과 크롤링

---
+++ Crawler/script/사범대학/생물교육과.js
@@ -0,0 +1,29 @@
+module.exports = {
+    url: "http://edu.chungbuk.ac.kr/bio/selectBbsNttList.do?key=399&bbsNo=2",
+    site_id: 40501,
+    getData: function getData() {
+        var list = document.querySelectorAll('#board > div.tableA > table > tbody > tr')
+        let i = 0;
+        let data = []
+        while (i < list.length) {
+            let td = list[i].querySelectorAll('td')
+            data.push({
+                site: "생물교육과",
+                category: "공지사항",
+                site_id:40501,
+                title: td[2].querySelector('a').innerText.trim(),
+                url: td[2].querySelector('a').href.trim(),
+                date: td[5].innerText.trim(),
+            })
+            i++
+        }
+        return data
+    },
+    getContentsHtml: function getContentsHtml(){
+        let IMG=document.querySelectorAll('img')
+        for(img of IMG){
+            img.src=img.src
+        }
+        return document.querySelector('#board > div > div.tit_area > ul > li:nth-child(4) > div').outerHTML
+    },
+}
---
+++ Crawler/script/사범대학/수학교육과.js
@@ -0,0 +1,29 @@
+module.exports = {
+    url: "http://edu.chungbuk.ac.kr/mathedu/selectBbsNttList.do?key=763&bbsNo=88",
+      site_id: 40601,
+    getData: function getData() {
+        var list = document.querySelectorAll('#board > div.tableA > table > tbody > tr')
+        let i = 0;
+        let data = []
+        while (i < list.length) {
+            let td = list[i].querySelectorAll('td')
+            data.push({
+                site: "수학교육과",
+                category: "공지사항",
+                site_id:40601,
+                title: td[1].querySelector('a').innerText.trim(),
+                url: td[1].querySelector('a').href.trim(),
+                date: td[4].innerText.trim(),
+            })
+            i++
+        }
+        return data
+    },
+    getContentsHtml: function getContentsHtml(){
+        let IMG=document.querySelectorAll('img')
+        for(img of IMG){
+            img.src=img.src
+        }
+        return document.querySelector('#board > div > div.tit_area > ul > li:nth-child(3) > div').outerHTML
+    },
+}
