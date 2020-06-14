b35cbc438e7b2095f60920e344f676664bce7518
jaryapp <jaryapp@gmail.com>
2020-06-15 01:14:34 +0900

    생물학과, 생화학과 크롤링

---
+++ Crawler/script/자연과학대학/생화학과.js
@@ -0,0 +1,31 @@
+module.exports = {
+    url: "http://biochem.chungbuk.ac.kr/info/data/data_list.asp?dev=501&part=",
+    site_id: 120401,
+    getData: function getData() {
+        var list = document.querySelectorAll('#cContents > center > table > tbody > tr > td > table:nth-child(2) > tbody > tr')
+        let i = 3;
+        let data = []
+        while (i < list.length) {
+            let td = list[i].querySelectorAll('td')
+            let urltmp=td[1].querySelector('a').toString().split("'")[1];
+            urltmp="http://biochem.chungbuk.ac.kr/info/data/data_view.asp?num="+urltmp+"&dev=501"
+            data.push({
+                site: "생화학과",
+                category: "공지사항",
+                site_id:120401,
+                title: td[1].innerText.trim(),
+                url: urltmp,
+                date: td[5].innerText.trim()
+            })
+            i=i+2;
+        }
+        return data
+    },
+    getContentsHtml: function getContentsHtml() {
+        let IMG = document.querySelectorAll('img')
+        for (img of IMG) {
+            img.src = img.src
+        }
+        return document.querySelector('#cContents > center > table > tbody > tr:nth-child(2) > td > center > table > tbody > tr:nth-child(3) > td').innerHTML
+    }
+}
