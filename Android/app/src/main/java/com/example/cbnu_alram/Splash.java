a3dd9355a2da7000221d56070a888beff6b09909
jaryapp <jaryapp@gmail.com>
2020-06-15 22:14:55 +0900

    어플 첫화면 Splash 화면 개발

---
+++ Android/app/src/main/java/com/example/cbnu_alram/Splash.java
@@ -0,0 +1,17 @@
+package com.example.cbnu_alram;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
