49518eb0fff19e333c1adbc84facbe22dcf2aac5
nohgijin <ynda11111@naver.com>
2020-06-16 22:48:53 +0900

    사이트 공지사항 전달 router

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -7,16 +7,10 @@ var { format } = require('mysql2')
 
 router.get('/api/notice', async (req, res) => {
   // console.log(req.query.site)
-  let q = format(`SELECT * FROM notice_detail_no_contents WHERE site_id in (${req.query.site}) order by date desc`)
+  let q = format(`SELECT * FROM notice_detail_no_contents WHERE site_id in (${req.query.site}) order by date desc, id desc limit 15 offset ${req.query.offset*15};`)
   // console.log(q)
   let data = await promisePool.query(q)
   rows = data[0]
-  res.json(rows)
-});
-
-router.get('/api/notice/detail', async (req, res) => {
-  [rows, fileds] = await promisePool.query(`SELECT * FROM notice_detail WHERE id = ${req.query.id};`)
-
   res.json(rows)
 });
 
@@ -31,6 +25,19 @@ router.get('/api/v2/notice', async (req, res) => {
   })
   res.json(rows)
 });
+
+router.get('/api/v2/site/notice', async (req, res) => {
+  // console.log(req.query.site)
+  let q = format(`SELECT * FROM notice_detail_no_contents WHERE category1 = '${req.query.site}' OR category2 = '${req.query.site}' order by date desc, id desc `)
+  console.log(q)
+  let data = await promisePool.query(q)
+  rows = data[0]
+  rows.forEach(v=>{
+    v.date = new Date(v.date).toLocaleDateString();
+  })
+  res.json(rows)
+});
+
 router.get('/api/v2/notice/major', async (req, res) => {
   // console.log(req.query.site)
   let q = format(`SELECT * FROM notice_detail_no_contents WHERE site_id in (SELECT site_id FROM push_allow INNER JOIN site ON push_allow.site_id = site.id WHERE fcm_token = "${req.headers.token}") and site_id < 140000 order by date desc, id desc`)
@@ -53,5 +60,144 @@ router.get('/api/v2/notice/common', async (req, res) => {
   })
   res.json(rows)
 });
+
+router.get('/api/notice/noticeId', async (req, res) => {
+  // console.log(req.query.site)
+  let q = format(`SELECT * FROM notice_detail_no_contents WHERE id in (${req.query.notice}) order by date desc,id desc limit 15 offset ${req.query.offset*15};`)
+  console.log(q)
+  let data = await promisePool.query(q)
+  rows = data[0]
+  res.json(rows)
+});
+
+router.get('/api/notice/detail', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM notice_detail WHERE id = ${req.query.id};`)
+
+  res.json(rows)
+});
+
+router.get('/api/v2/notice/detail', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM notice_detail WHERE id = ${req.query.id};`)
+
+  rows.forEach(v=>{
+    v.date = new Date(v.date).toLocaleDateString();
+  })
+
+
+  res.json(rows)
+});
+
+router.get('/api/notice/site', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM notice_detail order by site_id;`)
+
+  res.json(rows)
+});
+
+router.get('/api/site', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT site.category2 as title, site.id as site_id FROM site;`)
+
+  res.json(rows)
+});
+
+router.get('/api/site/all', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM site;`)
+
+  res.json(rows)
+});
+
+router.get('/api/allow', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM push_allow WHERE fcm_token = '${req.headers.token}'`)
+
+  rows = rows.map((item) => item.site_id)
+
+  res.json(rows)
+});
+
+router.get('/api/allow/major', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM push_allow WHERE fcm_token = '${req.headers.token}' and site_id < 140000`)
+
+  rows = rows.map((item) => item.site_id)
+
+  res.json(rows)
+});
+
+router.get('/api/allow/common', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM push_allow WHERE fcm_token = '${req.headers.token}' and site_id >= 140000`)
+
+  rows = rows.map((item) => item.site_id)
+
+  res.json(rows)
+});
+
+
+router.get('/api/allow/site', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT * FROM push_allow INNER JOIN site ON push_allow.site_id = site.id WHERE fcm_token = '${req.headers.token}'`)
+
+  res.json(rows)
+});
+
+router.get('/api/v2/allow/site', async (req, res) => {
+  [rows, fileds] = await promisePool.query(`SELECT DISTINCT category2 AS site FROM push_allow_detail WHERE fcm_token = "${req.headers.token}"`)
+
+  res.json(rows.map(data=>data.site))
+});
+
+router.post('/api/fcm_token', async (req, res) => {
+  // console.log('asfasf')
+  [rows, fileds] = await promisePool.query('SELECT * FROM user WHERE ?', {fcm_token:req.headers.token})
+  if (rows.length == 0) await promisePool.query('INSERT INTO user SET ?', {fcm_token:req.headers.token})
+  // console.log(req.body)
+  res.send()
+})
+
+router.post('/api/allow', async (req, res) => {
+  let data = {
+    fcm_token : req.headers.token,
+    site_id:req.body.site_id
+  }
+
+  console.log(data)
+
+  let result = await promisePool.query('INSERT INTO push_allow SET ?', data)
+
+  res.json(result)
+})
+
+router.post('/api/allow/site', async (req, res) => {
+
+  try {
+    let data = {
+      fcm_token : req.headers.token,
+      site_name:req.body.site_name
+    }
+  
+    console.log(data)
+  
+    let result = await promisePool.query(`INSERT INTO push_allow SET fcm_token = '${data.fcm_token}', site_id = (SELECT id as site_id FROM site WHERE category2="${data.site_name}" LIMIT 1) `)
+  
+    res.json(result)
+  } catch (error) {
+    res.sendStatus(404)
+  }
+
+
+
+})
+
+router.delete('/api/allow', async (req, res) => {
+  let result = await promisePool.query('DELETE FROM push_allow WHERE fcm_token = ? and site_id = ?', [req.headers.token, req.query.site_id])
+  res.json(result)
+})
+
+router.delete('/api/v2/allow', async (req, res) => {
+  try {
+    let result = await promisePool.query('DELETE FROM push_allow WHERE fcm_token = ? and site_id = (SELECT id as site_id FROM site WHERE category2= ? LIMIT 1) ', [req.headers.token, req.query.site_name])
+    res.json(result) 
+  } catch (error) {
+    res.sendStatus(404)
+  }
+})
+
+
 
 module.exports = router;
